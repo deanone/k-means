9e8fe8096b32b08b81e8f161cb05cbab7b1bc90b
Athanasios Salamanis <asal@iti.gr>
2019-07-18 16:21:34 +0300

    Refactored PropertiesParser class.

--- PropertiesParser.cpp
+++ PropertiesParser.cpp
@@ -1,3 +1,6 @@
+#include <fstream>
+#include <sstream>
+
 #include "PropertiesParser.h"
 
 PropertiesParser::PropertiesParser(std::string propertiesFilename)
@@ -9,47 +12,41 @@ void PropertiesParser::addPropertiesFile(std::string propertiesFilename)
 {
 	std::ifstream properties_file;
 	properties_file.open(propertiesFilename.c_str(), std::ios::in);
-	if (properties_file.is_open())
+	std::string line;
+	if (properties_file.good())
 	{
-		std::string line;
-		if (properties_file.good())
+		while (properties_file.eof() == false)
 		{
-			while (properties_file.eof() == false)
+			line.clear();
+			getline(properties_file, line);
+			if (line.length() > 0 && line[0] != '#')
 			{
-				line.clear();
-				getline(properties_file, line);
-				if (line.length() > 0 && line[0] != '#')
-				{
-					std::string propertyName;
-					std::string propertyValue;
-					std::stringstream ss(line);
-					char c;//c used to eat the '=' character
-					ss >> propertyName >> c >> propertyValue;
-					//std::cout << propertyName << " = " << propertyValue << "\n"; 
-					PropMap[propertyName] = propertyValue;
-				}
+				std::string propertyName;
+				std::string propertyValue;
+				std::stringstream ss(line);
+				char c;//c used to eat the '=' character
+				ss >> propertyName >> c >> propertyValue;
+				propMap[propertyName] = propertyValue;
 			}
 		}
-		properties_file.close();
 	}
-	else
-		std::cout << "Cannot open file.\n";
+	properties_file.close();
 }
 
 bool PropertiesParser::propertyExists(std::string propertyName)
 {
-	return (!(PropMap.find(propertyName) == PropMap.end()));
+	return (!(propMap.find(propertyName) == propMap.end()));
 }
 
 std::string PropertiesParser::getPropertyAsString(std::string propertyName)
 {
-	return PropMap[propertyName];
+	return propMap[propertyName];
 }
 
 int PropertiesParser::getPropertyAsInt(std::string propertyName)
 {
 	int propertyValue;
-	std::stringstream strStream(PropMap[propertyName]);
+	std::stringstream strStream(propMap[propertyName]);
 	strStream >> propertyValue;
 	return propertyValue;
 }
@@ -57,14 +54,14 @@ int PropertiesParser::getPropertyAsInt(std::string propertyName)
 bool PropertiesParser::getPropertyAsBool(std::string propertyName)
 {
 	bool propertyValue;
-	propertyValue = (PropMap[propertyName] == "true");
+	propertyValue = (propMap[propertyName] == "true");
 	return propertyValue;
 }
 
-float PropertiesParser::getPropertyAsFloat(std::string propertyName)
+double PropertiesParser::getPropertyAsFloat(std::string propertyName)
 {
-	float propertyValue;
-	std::stringstream strStream(PropMap[propertyName]);
+	double propertyValue;
+	std::stringstream strStream(propMap[propertyName]);
 	strStream >> propertyValue;
 	return propertyValue;
 }
@@ -72,7 +69,7 @@ float PropertiesParser::getPropertyAsFloat(std::string propertyName)
 double PropertiesParser::getPropertyAsDouble(std::string propertyName)
 {
 	double propertyValue;
-	std::stringstream strStream(PropMap[propertyName]);
+	std::stringstream strStream(propMap[propertyName]);
 	strStream >> propertyValue;
 	return propertyValue;
 }
@@ -80,39 +77,59 @@ double PropertiesParser::getPropertyAsDouble(std::string propertyName)
 std::string PropertiesParser::getPropertyAsStringOrDefaultTo(std::string propertyName, std::string defaultPropertyValue)
 {
 	if (propertyExists(propertyName))
+	{
 		return getPropertyAsString(propertyName);
+	}
 	else
+	{
 		return defaultPropertyValue;
+	}
 }
 
 int PropertiesParser::getPropertyAsIntOrDefaultTo(std::string propertyName, int defaultPropertyValue)
 {
 	if (propertyExists(propertyName))
+	{
 		return getPropertyAsInt(propertyName);
+	}
 	else
+	{
 		return defaultPropertyValue;
+	}
 }
 
 bool PropertiesParser::getPropertyAsBoolOrDefaultTo(std::string propertyName, bool defaultPropertyValue)
 {
 	if (propertyExists(propertyName))
+	{
 		return getPropertyAsBool(propertyName);
+	}
 	else
+	{
 		return defaultPropertyValue;
+	}
 }
 
-float PropertiesParser::getPropertyAsFloatOrDefaultTo(std::string propertyName, float defaultPropertyValue)
+double PropertiesParser::getPropertyAsFloatOrDefaultTo(std::string propertyName, double defaultPropertyValue)
 {
 	if (propertyExists(propertyName))
+	{
 		return getPropertyAsFloat(propertyName);
+	}
 	else
+	{
 		return defaultPropertyValue;
+	}
 }
 
 double PropertiesParser::getPropertyAsDoubleOrDefaultTo(std::string propertyName, double defaultPropertyValue)
 {
 	if (propertyExists(propertyName))
+	{
 		return getPropertyAsDouble(propertyName);
+	}
 	else
+	{
 		return defaultPropertyValue;
+	}
 }
--- PropertiesParser.h
+++ PropertiesParser.h
@@ -1,26 +1,97 @@
 #ifndef PROPERTIES_PARSER_H
 #define PROPERTIES_PARSER_H
 
-#include "DataTypes.h"
+#include <map>
 
 class PropertiesParser
 {
 protected:
 	/*!A map containing property names as keys and property values as values.*/
-	std::map<std::string, std::string> PropMap;
+	std::map<std::string, std::string> propMap;
+
 public:
+	/*!
+	 * Constructor.
+	 */
 	PropertiesParser(std::string propertiesFilename);
+	
 	void addPropertiesFile(std::string propertiesFilename);
+	
 	bool propertyExists(std::string propertyName);
+	
+	/*!
+	 * Getter of a property as type string.
+	 * @ param propertyName the name of the property.
+	 * @return the string type property.
+	 */
 	std::string getPropertyAsString(std::string propertyName);
+	
+	/*!
+	 * Getter of a property as type int.
+	 * @ param propertyName the name of the property.
+	 * @return the int type property.
+	 */
 	int getPropertyAsInt(std::string propertyName);
+	
+	/*!
+	 * Getter of a property as type bool.
+	 * @ param propertyName the name of the property.
+	 * @return the bool type property.
+	 */
 	bool getPropertyAsBool(std::string propertyName);
-	float getPropertyAsFloat(std::string propertyName);
+	
+	/*!
+	 * Getter of a property as type float.
+	 * @ param propertyName the name of the property.
+	 * @return the float type property.
+	 */
+	double getPropertyAsFloat(std::string propertyName);
+	
+	/*!
+	 * Getter of a property as type double.
+	 * @ param propertyName the name of the property.
+	 * @return the double type property.
+	 */
 	double getPropertyAsDouble(std::string propertyName);
+	
+	/*!
+	 * Getter of a property as type string, or return a default string type value.
+	 * @ param propertyName the name of the property.
+	 * @ param defaultPropertyValue the default value of the property
+	 * @return the string type property, or a default string type value.
+	 */
 	std::string getPropertyAsStringOrDefaultTo(std::string propertyName, std::string defaultPropertyValue);
+	
+	/*!
+	 * Getter of a property as type int, or return a default int type value.
+	 * @ param propertyName the name of the property.
+	 * @ param defaultPropertyValue the default value of the property
+	 * @return the int type property, or a default int type value.
+	 */
 	int getPropertyAsIntOrDefaultTo(std::string propertyName, int defaultPropertyValue);
+	
+	/*!
+	 * Getter of a property as type bool, or return a default bool type value.
+	 * @ param propertyName the name of the property.
+	 * @ param defaultPropertyValue the default value of the property
+	 * @return the bool type property, or a default bool type value.
+	 */
 	bool getPropertyAsBoolOrDefaultTo(std::string propertyName, bool defaultPropertyValue);
-	float getPropertyAsFloatOrDefaultTo(std::string propertyName, float defaultPropertyValue);
+	
+	/*!
+	 * Getter of a property as type float, or return a default float type value.
+	 * @ param propertyName the name of the property.
+	 * @ param defaultPropertyValue the default value of the property
+	 * @return the int float property, or a default float type value.
+	 */
+	double getPropertyAsFloatOrDefaultTo(std::string propertyName, double defaultPropertyValue);
+	
+	/*!
+	 * Getter of a property as type double, or return a default double type value.
+	 * @ param propertyName the name of the property.
+	 * @ param defaultPropertyValue the default value of the property
+	 * @return the double type property, or a default double type value.
+	 */
 	double getPropertyAsDoubleOrDefaultTo(std::string propertyName, double defaultPropertyValue);
 };
 
